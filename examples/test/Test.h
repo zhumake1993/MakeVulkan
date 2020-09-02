94a1863475ed2c925493b3a9975362840c5be6a8
zhumake <zhumake1993@gmail.com>
2020-09-02 18:42:44 +0800

    支持多目标构建

---
+++ examples/test/Test.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "Engine.h"
+#include "VulkanFwd.h"
+#include "EngineFwd.h"
+
+class Test : public Engine
+{
+	struct UniformBuffer
+	{
+		alignas(16) glm::mat4 view;
+		alignas(16) glm::mat4 proj;
+	};
+
+public:
+
+	Test();
+	virtual ~Test();
+
+protected:
+
+	void CleanUp() override;
+	void Init() override;
+	void Tick() override;
+	void RecordCommandBuffer(VulkanCommandBuffer* vulkanCommandBuffer) override;
+
+private:
+
+	void PrepareModels();
+	void PrepareTextures();
+	void PrepareDescriptorSet();
+	void CreatePipeline();
+
+private:
+
+	// Mesh
+	Mesh* m_Mesh;
+
+	// Resource
+	VulkanImage* m_Image;
+
+	// DescriptorSet
+	VulkanDescriptorPool* m_VulkanDescriptorPool;
+	VulkanDescriptorSetLayout* m_VulkanDescriptorSetLayout;
+	VulkanDescriptorSet* m_VulkanDescriptorSet;
+
+	// Render Status
+	VulkanPipeline* m_VulkanPipeline;
+	VulkanPipelineLayout* m_VulkanPipelineLayout;
+	VulkanRenderPass* m_VulkanRenderPass;
+
+	// Uniform Buffer
+	UniformBuffer m_UniformBuffer;
+
+	// camera
+	Camera* m_Camera;
+
+	// stats
+	uint32_t m_FrameIndex = 0;
+	uint32_t m_AccumulateCounter = 0;
+	float m_AccumulateTime = 0;
+	float m_FPS = 0;
+	std::chrono::time_point<std::chrono::high_resolution_clock> lastTimestamp = std::chrono::high_resolution_clock::now();
+};
